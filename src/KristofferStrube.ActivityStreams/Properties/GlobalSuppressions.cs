df504c92034629735ee1ba485b83211f2641695a
Werner Mairl <werner.mairl@gmail.com>
2024-12-09 07:55:47 +0100

    nowarn

--- src/KristofferStrube.ActivityStreams/Properties/GlobalSuppressions.cs
+++ src/KristofferStrube.ActivityStreams/Properties/GlobalSuppressions.cs
@@ -1,2 +1,3 @@
 using System.Diagnostics.CodeAnalysis;
 //[assembly: ExcludeFromCodeCoverage]
+[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "PROTOTYPE", Scope = "namespace", Target = "~N:KristofferStrube.ActivityStreams")]
